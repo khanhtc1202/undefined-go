cca1db270f7d7b27bdbb8ad57426097bb47ab335
khanhub <khanh.tran@uzabase.com>
2018-11-14 17:25:26 +0900

    test

--- sample/comp/multiple.go
+++ sample/comp/multiple.go
@@ -33,3 +33,25 @@ func NonBufferChanGoRoutine(m []MultiplePair) (sum int64) {
 		}
 	}
 }
+
+func BufferChanGoRoutine(m []MultiplePair) (sum int64) {
+	mul := make(chan int64, len(m))
+	for _, p := range m {
+		go func(p MultiplePair) {
+			r := p.x * p.y
+			mul <- r
+		}(p)
+	}
+
+	counter := 0
+	for {
+		select {
+		case r := <-mul:
+			sum += r
+			counter++
+			if counter == len(m) {
+				return
+			}
+		}
+	}
+}
--- sample/comp/multiple_test.go
+++ sample/comp/multiple_test.go
@@ -18,6 +18,12 @@ func BenchmarkNonBufferChanGoRoutine(b *testing.B) {
 	}
 }
 
+func BenchmarkBufferChanGoRoutine(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		BufferChanGoRoutine(randomPairs())
+	}
+}
+
 func randomPairs() []MultiplePair {
 	var pairs []MultiplePair
 	for i := 0; i < 1000; i++ {
