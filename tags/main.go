525d310479103e4c779d74a0149401ff87e92f6c
khanhub <khanh.tran@uzabase.com>
2018-12-03 23:28:01 +0900

    map

--- tags/main.go
+++ tags/main.go
@@ -1,1 +1,16 @@
 package main
+
+import "reflect"
+
+type TypeMapper interface {
+	Map(interface{}) TypeMapper
+}
+
+type injector struct {
+	values map[reflect.Type]reflect.Value
+}
+
+func (i *injector) Map(val interface{}) TypeMapper {
+	i.values[reflect.TypeOf(val)] = reflect.ValueOf(val)
+	return i
+}
