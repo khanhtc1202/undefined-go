ca3e3057ba7d093d141034ceb1168b9e7078c31e
khanhtc1202 <khanhtc1202@gmail.com>
2019-08-13 17:26:35 +0900

    fs

--- containers/main.go
+++ containers/main.go
@@ -13,23 +13,37 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		panic("what?")
 	}
 }
 
 func run() {
-	fmt.Printf("running %v\n", os.Args[2:])
+
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+	}
+
+	must(cmd.Run())
+}
+
+func child() {
+	fmt.Printf("running %v as pid %d\n", os.Args[2:], os.Getpid())
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
-	}
-
+	must(syscall.Chroot("/app/rootfs"))
+	must(os.Chdir("/"))
 	must(cmd.Run())
 }
 
