40a4b6c6ec50d05e68e31d6020cf7f5cf5c227bf
khanhtc1202 <khanhtc1202@gmail.com>
2019-12-09 22:13:28 +0700

    remove fmt

--- concurrency/chann.go
+++ concurrency/chann.go
@@ -1,13 +1,11 @@
 package main
 
-import "fmt"
-
 func main() {
     done := make(chan int, 1)
 
     go func(){
 	for i := 0; i < 10; i++ {
-		fmt.Println("Hello World")
+		println("Hello World")
 		done <- 1
 	}
     }()
