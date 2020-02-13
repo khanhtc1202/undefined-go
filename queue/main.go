66ed261549ca2e2e593bfe90b14a15be793eb5b5
khanhtc1202 <khanhtc1202@gmail.com>
2020-02-13 20:08:40 +0900

    worker with close on done

--- queue/main.go
+++ queue/main.go
@@ -2,22 +2,30 @@ package main
 
 import (
 	"log"
+	"time"
 )
 
-func worker(queue chan int, workernumber int, done chan bool) {
-	for j := range queue {
-		log.Print("worker ", workernumber, " done with job ", j)
-		done <- true
+func worker(queue chan int, workernumber int, done chan bool, killsignal chan bool) {
+	for {
+		select {
+		case k := <-queue:
+			log.Print("worker ", workernumber, " done with job ", k)
+			done <- true
+		case <-killsignal:
+			log.Print("worker halted, number", workernumber)
+			return
+		}
 	}
 }
 
 func main() {
+	kill := make(chan bool)
 	q := make(chan int)
 	done := make(chan bool)
 
 	numberOfWorkers := 4
 	for i := 0; i < numberOfWorkers; i++ {
-		go worker(q, i, done) // waiting for jobs
+		go worker(q, i, done, kill) // waiting for jobs
 	}
 
 	numberOfJobs := 17
@@ -31,4 +39,10 @@ func main() {
 	for c := 0; c < numberOfJobs; c++ {
 		<-done
 	}
+
+	close(kill)
+	// we need this to make sure program wait until kill signal be passed
+	// NOTE that defer not work here since the program can be halted before defer block done
+	// otherwise use waitgroup if you don't want to use sleep here
+	time.Sleep(2 * time.Second) 
 }
