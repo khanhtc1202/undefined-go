c35029917afe26f9613ab01fbf67f53a32b2b686
khanhtc1202 <khanhtc1202@gmail.com>
2020-07-27 00:32:51 +0900

    spinner

---
+++ prog-book/chap-8/spinner/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func spinner(delay time.Duration) {
+	for {
+		for _, r := range `-\|/` {
+			fmt.Printf("\r%c", r)
+			time.Sleep(delay)
+		}
+	}
+}
+
+func main() {
+	fmt.Println("Hello, playground")
+	spinner(100 * time.Millisecond)
+}
